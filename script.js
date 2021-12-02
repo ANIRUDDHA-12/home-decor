64a691d6832a967700098eba20cb9ab48d80d4a7
anirudh <payas.sanjay@gmail.com>
2021-12-02 15:28:54 +0530

    done

---
+++ script.js
@@ -0,0 +1,14 @@
+let cart = document.getElementById("cart")
+let container = document.getElementById("cart-container");
+cart.addEventListener("click",()=>{
+container.style.display = "block"
+console.log("anirudh")
+})
+
+let close = document.getElementById("close")
+close.addEventListener("click",()=>{
+    container.style.display = "none"
+    console.log("anirudh")
+    })
+
+
